a1728cc070f7267608e91353dccec5de7eb4fe2f
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-11-11 17:32:45 +0530

    Remove calls to get coin list & category list

--- Coinmarketcal.js
+++ Coinmarketcal.js
@@ -5,8 +5,6 @@ const cache = require('memory-cache');
 const coinsList = require('./coins.json');
 const categoriesList = require('./categories.json');
 
-const WEEK_IN_MS = (7 * 24 * 60 * 60 * 1000);
-
 module.exports = class CoinMarketCalendarClient {
   constructor({ clientId, clientSecret }) {
     this.clientId = clientId;
@@ -47,94 +45,6 @@ module.exports = class CoinMarketCalendarClient {
       }
     }
   }
-
-
-  async getCoins() {
-    // const cachedCoinList = cache.get('coins');
-    // if (cachedCoinList && Array.isArray(cachedCoinList) && cachedCoinList.length > 0) {
-    //   return cachedCoinList;
-    // }
-
-
-    // const coinsUrl = 'https://api.coinmarketcal.com/v1/coins';
-
-    // if (this.accessToken == null) {
-    //   await this.authenticate();
-    // }
-
-    // try {
-    //   const response = await axios(coinsUrl, {
-    //     params: {
-    //       access_token: this.accessToken,
-    //     },
-    //   });
-
-    //   const coins = response.data;
-
-    //   if (coins && Array.isArray(coins) && coins.length > 0) {
-    //     cache.put('coins', coins, WEEK_IN_MS);
-    //     return coins;
-    //   }
-    // } catch (e) {
-    //   if (e.response && e.response.status === 401) {
-    //     console.log('Authentication failed. Try again.');
-    //     this.accessToken = null;
-    //     cache.del('access_token');
-    //   } else if (e.code === 'ENOTFOUND') {
-    //     console.log('Unable to connect to server. Check your internet connection');
-    //   } else {
-    //     console.log(e);
-    //   }
-    // }
-    //
-    // return null
-
-    return coinsList;
-  }
-
-
-  async getCategories() {
-    // const cachedCategoryList = cache.get('categories');
-    // if (cachedCategoryList && Array.isArray(cachedCategoryList) && cachedCategoryList.length > 0) {
-    //   return cachedCategoryList;
-    // }
-
-    // const categoriesUrl = 'https://api.coinmarketcal.com/v1/categories';
-
-    // if (this.accessToken == null) {
-    //   await this.authenticate();
-    // }
-
-    // try {
-    //   const response = await axios(categoriesUrl, {
-    //     params: {
-    //       access_token: this.accessToken,
-    //     },
-    //   });
-
-    //   const categories = response.data;
-
-    //   if (categories && Array.isArray(categories) && categories.length > 0) {
-    //     cache.put('categories', categories, WEEK_IN_MS);
-    //     return categories;
-    //   }
-    // } catch (e) {
-    //   if (e.response && e.response.status === 401) {
-    //     console.log('Authentication failed. Try again.');
-    //     this.accessToken = null;
-    //     cache.del('access_token');
-    //   } else if (e.code === 'ENOTFOUND') {
-    //     console.log('Unable to connect to server. Check your internet connection');
-    //   } else {
-    //     console.log(e);
-    //   }
-    // }
-
-    // return null;
-
-    return categoriesList;
-  }
-
 
   async getEvents({
     page = 1, max = 150, coins, categories,
@@ -181,9 +91,7 @@ module.exports = class CoinMarketCalendarClient {
       return [];
     }
 
-    const coinList = await this.getCoins();
-
-    return coinList
+    return coinsList
       .filter(coin => coinSymbols.includes(coin.symbol.toUpperCase()))
       .map(coin => coin.id);
   }
