52e80dd112c5228cba08c3903d25d8ea4cd8aaea
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2019-03-30 17:03:04 +0530

    Log API key & telegram token from env instead of secrets file

--- index.js
+++ index.js
@@ -2,26 +2,24 @@ const CoinMarketCalendarClient = require('./Coinmarketcal');
 const { initializeResponseHandler } = require('./bot');
 const axios = require('axios');
 
-const {
-  apiKey,
-  telegramToken,
-} = require('./secrets.json');
+const API_KEY = process.env.API_KEY
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
 
+const coinmarketcalApi = new CoinMarketCalendarClient({ API_KEY });
 
-const coinmarketcalApi = new CoinMarketCalendarClient({ apiKey });
-
-const RESPONSE_URL = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
+const RESPONSE_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
 const responseHandler = initializeResponseHandler(coinmarketcalApi);
 
 exports.handler = async (event) => {
   const messageText = event.message.text;
   const chatId = event.message.chat.id;
-  const responseMsg = {
-    text: (await responseHandler(messageText)),
-    chat_id: chatId,
-  };
 
-  await axios.post(RESPONSE_URL, responseMsg);
+  const responseText = await responseHandler(messageText);
+
+  await axios.post(RESPONSE_URL, {
+    text: responseText,
+    chat_id: chatId,
+  });
 
   return {
     statusCode: 200,
